ecff0fce02564e54c45bb380fa46ce2a8c951e27
aliraad79 <a.ahmadi.k.79@gmail.com>
2019-11-07 18:29:44 +0330

    first 6

--- base/forms.py
+++ base/forms.py
@@ -1,8 +1,9 @@
 from django import forms
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
 
-class SignUpForm(forms.Form):
+class SignUpForm(UserCreationForm):
     first_name = forms.CharField(max_length=30, required=False)
     last_name = forms.CharField(max_length=30, required=False)
     user_name = forms.CharField(max_length=30, required=True)
