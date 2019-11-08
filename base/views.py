0dda89e23795e8534af7f01a38b5738bf678c00a
MohsenDehghankar <45297389+MohsenDehghankar@users.noreply.github.com>
2019-11-08 11:41:57 +0330

    bacj

--- base/views.py
+++ base/views.py
@@ -130,21 +130,23 @@ def edit_profile_view(request):
 def all_courses_view(request):
     data = None
     if request.method == "POST":
-        depart = request.POST.get('department') == 'department'
-        teacher = request.POST.get('teacher') == 'teacher'
-        course = request.POST.get('course') == 'course'
         query = request.POST["search_query"]
-        if not depart and not course and not teacher:
-            data = Course.objects.filter(department=query)
-        else:
-            data1 = Course.objects.none()
-            data2 = Course.objects.none()
-            data3 = Course.objects.none()
-            if depart:
-                data1 = Course.objects.filter(department=query)
-            if teacher:
-                data2 = Course.objects.filter(teacher=query)
-            if course:
-                data3 = Course.objects.filter(name=query)
-            data = (data1 | data2 | data3).distinct()
+        data = Course.objects.filter(department=query)
+        # depart = request.POST.get('department') == 'department'
+        # teacher = request.POST.get('teacher') == 'teacher'
+        # course = request.POST.get('course') == 'course'
+        # query = request.POST["search_query"]
+        # if not depart and not course and not teacher:
+        #     data = Course.objects.filter(department=query)
+        # else:
+        #     data1 = Course.objects.none()
+        #     data2 = Course.objects.none()
+        #     data3 = Course.objects.none()
+        #     if depart:
+        #         data1 = Course.objects.filter(department=query)
+        #     if teacher:
+        #         data2 = Course.objects.filter(teacher=query)
+        #     if course:
+        #         data3 = Course.objects.filter(name=query)
+        #     data = (data1 | data2 | data3).distinct()
     return render(request, 'all_courses.html', {'courses': data})
