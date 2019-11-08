5bf0fe1eb0eb3fae26b77c6f1b076dac9f109b24
MohsenDehghankar <45297389+MohsenDehghankar@users.noreply.github.com>
2019-11-08 17:11:47 +0330

    sadfasf

--- base/views.py
+++ base/views.py
@@ -130,25 +130,24 @@ def all_courses_view(request):
     if request.method == "POST":
         query = request.POST["search_query"]
         data = Course.objects.filter(department=query)
-        # depart = request.POST.get('department') == 'department'
-        # teacher = request.POST.get('teacher') == 'teacher'
-        # course = request.POST.get('course') == 'course'
-        # query = request.POST["search_query"]
-        # if not depart and not course and not teacher:
-        #     data = Course.objects.filter(department=query)
-        # else:
-        #     data1 = Course.objects.none()
-        #     data2 = Course.objects.none()
-        #     data3 = Course.objects.none()
-        #     if depart:
-        #         data1 = Course.objects.filter(department=query)
-        #     if teacher:
-        #         data2 = Course.objects.filter(teacher=query)
-        #     if course:
-        #         data3 = Course.objects.filter(name=query)
-        #     data = (data1 | data2 | data3).distinct()
+        depart = request.POST.get('department') == 'department'
+        teacher = request.POST.get('teacher') == 'teacher'
+        course = request.POST.get('course') == 'course'
+        query = request.POST["search_query"]
+        if not depart and not course and not teacher:
+            data = Course.objects.filter(department=query)
+        else:
+            data1 = Course.objects.none()
+            data2 = Course.objects.none()
+            data3 = Course.objects.none()
+            if depart:
+                data1 = Course.objects.filter(department=query)
+            if teacher:
+                data2 = Course.objects.filter(teacher=query)
+            if course:
+                data3 = Course.objects.filter(name=query)
+            data = (data1 | data2 | data3).distinct()
     alldata = Course.objects.all()
-    # usecourses = getusercourses(request.user.username)
     return render(request, 'all_courses.html', {'courses': data, 'alldata': alldata})
 
 
